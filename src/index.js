72226e80da2afba704fc7f7465b85ddc3355c3fc
Aida Real <areal060781@gmail.com>
2018-07-12 19:28:29 -0500

    YouTube search response

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const API_KEY = 'AIzaSyCkhOlSjZ1x3u5CjF5Gsz9u7PBH7nLLm7s';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyCkhOlSjZ1x3u5CjF5Gsz9u7PBH7nLLm7s';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    console.log(data);
+});
 
 const App = () => {
     return (
